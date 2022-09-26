c3ac688556a0c93e10948fd4fef8ec9c9bea4e62
Алексей Калюжный <zero2502@gmail.com>
2022-09-26 18:01:53 +0300

    опреелить выходй ли день

---
+++ домашняя работа/task1.py
@@ -0,0 +1,7 @@
+num= int(input('Введите число: '))
+if num==6 or num ==7:
+    print("да")
+elif num==1 or num==2 or num==3 or num ==4 or num ==5:
+    print('нет')
+else: print('введено некорректное число')
+
