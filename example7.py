b9676cf4fc7e28564e36ff419175cc59b4430e40
Алексей Калюжный <zero2502@gmail.com>
2022-09-28 19:09:11 +0300

    образцовое решение

--- example7.py
+++ example7.py
@@ -6,3 +6,12 @@ for i in range(1, int(n)+1):
     elem=[i,3*i+1]
     result.append(elem)
 print(dict(result))  # dict- тип переменной для словаря
+
+
+# образцовый вариант решения
+number = input(" Введите число N")
+result=dict()
+for i in range (1,int(number)+1):
+
+    result[i]=3+i+1
+print(result)
