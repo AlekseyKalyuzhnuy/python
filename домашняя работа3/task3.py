46c0076daa00d1e9ce161feb9e538d73af8f7648
Алексей Калюжный <zero2502@gmail.com>
2022-10-06 15:50:56 +0300

    task3

---
+++ домашняя работа3/task3.py
@@ -0,0 +1,24 @@
+#Задайте список из вещественных чисел. Напишите программу, 
+# которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+n = int(input('Введите размер списка: '))
+result=[]
+for i in range(0, int (n)):
+    elem=float(input('Введите число: '))
+    result.append(elem)
+print(result)
+
+result2=[]
+for i in range(0, int (n)):
+    elem=round((result[i]-int(result[i])),5)
+    result2.append(elem)
+print(result2)
+
+max=result2[0]
+min=result2[0]
+for i in range(0, int (n)):
+
+
+
+
+
