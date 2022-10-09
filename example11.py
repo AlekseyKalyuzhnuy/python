0efb599c91a0d7992e0733c080c0ca7b0aa75381
Алексей Калюжный <zero2502@gmail.com>
2022-10-09 12:41:12 +0300

    преобразование строки в лист

--- example11.py
+++ example11.py
@@ -1,6 +1,11 @@
 #   задать строку из набора чисел. В качества символа-разделителя используйте пробел
 #   найти большее и меньшее число
 
-k="1 2 3 5 7 9 45"
-result = list(map(int, (k.split())))
-print(max(result), min(result))
+#k="1 2 3 5 7 9 45"
+#result = list(map(int, (k.split())))
+#print(max(result), min(result))
+
+# второе решение
+nums = '5 67 3 4 89 345 2 7 45'
+my_list = [int(num) for num in nums.split()]
+print(min(my_list), max(my_list))
