1e0c0406f323809364e574dca8c7b103dd53d410
Алексей Калюжный <zero2502@gmail.com>
2022-10-10 17:04:56 +0300

    два решения с точность числа Пи

---
+++ домашняя работа4/task1.py
@@ -0,0 +1,13 @@
+#Вычислить число c заданной точностью d
+# Пример:- при $d = 0.001, π = 3.141.$
+
+from math import pi
+d = float(input('Введите размер точности: '))
+k=1
+while d*(10**k)<1:
+  k+=1
+print(round(pi,k))
+
+# второй вариант
+#d=len(str(d).split('.')[1])
+#print(round(pi,d))
