1851fc8ba0deb50dcf1785d7545553c9beb3d3a6
Алексей Калюжный <zero2502@gmail.com>
2022-10-12 13:25:42 +0300

    пример создания списка, использования filter и lambda

--- function/1.py
+++ function/1.py
@@ -2,8 +2,13 @@
 data = list(map(int, input().split()))
 print(data)
 
-res = filter(lambda x: not x%2, data)
-res = list(map(lambda x: (x, x**2),res))
+# пример создания готового списка (ввод сразу в условие)
+#data = '1 2 3 5 8 15 38'.split()
+#res = list(map(int, data))
+
+
+#res = filter(lambda x: not x%2, data)  # задаем фильтр "только четные"
+#res = list(map(lambda x: (x, x**2),res))  # формируем список кортежей по условию
 print(res)
 
 
