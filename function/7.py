b131eb073ed8b3bfe486f69854a79e8194b1d2c9
Алексей Калюжный <zero2502@gmail.com>
2022-10-15 21:15:34 +0300

    получить список уникальных элементов

--- function/7.py
+++ function/7.py
@@ -4,6 +4,6 @@
 my_list = [1, 2, 3, 5, 1, 5, 3, 10 ]
 new_list = []
 for i in range(0, len(my_list)):
-    if my_list.count(my_list[i])==1:
+    if my_list.count(my_list[i])==1:  # ввели счетчик повторения элементов
         new_list.append(my_list[i])
 print(new_list)
