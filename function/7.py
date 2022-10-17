b85197a2f9388494e417f5db4639a1d3b5774c8f
Алексей Калюжный <zero2502@gmail.com>
2022-10-17 17:21:00 +0300

    lambda, filter,map

--- function/7.py
+++ function/7.py
@@ -7,3 +7,10 @@ for i in range(0, len(my_list)):
     if my_list.count(my_list[i])==1:  # ввели счетчик повторения элементов
         new_list.append(my_list[i])
 print(new_list)
+
+
+# второй вариант
+#my_list = list(map(int,input("input list:").split()))
+
+#list_unique = list(filter(lambda item: my_list.count(item) ==1, my_list))
+print(list_unique)
