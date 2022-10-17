d5fe8abeaa79fc163a05d00e3d4b6f407ab791c7
Алексей Калюжный <zero2502@gmail.com>
2022-10-17 17:31:24 +0300

    list comprehension

---
+++ домашняя работа6/task2.py
@@ -0,0 +1,13 @@
+#Задайте список из вещественных чисел. Напишите программу, 
+# которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+
+n = int(input('Введите размер списка: '))
+result=[]
+for i in range(n):
+    result.append(float(input('Введите число: ')))
+# print(result)
+
+new_list = [round(i % 1 ,2) for i in result if i % 1 != 0]
+print(new_list)
+print(f'{result} => {max(new_list)-min(new_list)}')
